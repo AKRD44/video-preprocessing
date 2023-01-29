7f8e7f6056e52fb4fc76df3360b72d21696aa92b
ak <alex.y.keenan@gmail.com>
2023-01-29 09:10:15 -0500

    getting my laptop to do this stuff

--- load_videos.py
+++ load_videos.py
@@ -13,11 +13,13 @@ from util import save
 from argparse import ArgumentParser
 from skimage import img_as_ubyte
 from skimage.transform import resize
+import youtube_dl
 warnings.filterwarnings("ignore")
 
 DEVNULL = open(os.devnull, 'wb')
 
 def download(video_id, args):
+    print(f"doing {video_id}")
     video_path = os.path.join(args.video_folder, video_id + ".mp4")
     subprocess.call([args.youtube, '-f', "''best/mp4''", '--write-auto-sub', '--write-sub',
                      '--sub-lang', 'en', '--skip-unavailable-fragments',
@@ -25,63 +27,77 @@ def download(video_id, args):
                      video_path], stdout=DEVNULL, stderr=DEVNULL)
     return video_path
 
+# def download(video_id, args):
+#     video_path = os.path.join(args.video_folder, video_id + ".mp4")
+#     ydl_opts = {}
+#     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#         ydl.download(["https://www.youtube.com/watch?v=" + video_id])
+#     return video_path
+
 
 def run(data):
-    video_id, args = data
-    if not os.path.exists(os.path.join(args.video_folder, video_id.split('#')[0] + '.mp4')):
-       download(video_id.split('#')[0], args)
-
-    if not os.path.exists(os.path.join(args.video_folder, video_id.split('#')[0] + '.mp4')):
-       print ('Can not load video %s, broken link' % video_id.split('#')[0])
-       return 
-    reader = imageio.get_reader(os.path.join(args.video_folder, video_id.split('#')[0] + '.mp4'))
-    fps = reader.get_meta_data()['fps']
-
-    df = pd.read_csv(args.metadata)
-    df = df[df['video_id'] == video_id]
-    
-    all_chunks_dict = [{'start': df['start'].iloc[j], 'end': df['end'].iloc[j],
-                        'bbox': list(map(int, df['bbox'].iloc[j].split('-'))), 'frames':[]} for j in range(df.shape[0])]
-    ref_fps = df['fps'].iloc[0]
-    ref_height = df['height'].iloc[0]
-    ref_width = df['width'].iloc[0]
-    partition = df['partition'].iloc[0]
     try:
-        for i, frame in enumerate(reader):
-            for entry in all_chunks_dict:
-                if (i * ref_fps >= entry['start'] * fps) and (i * ref_fps < entry['end'] * fps):
-                    left, top, right, bot = entry['bbox']
-                    left = int(left / (ref_width / frame.shape[1]))
-                    top = int(top / (ref_height / frame.shape[0]))
-                    right = int(right / (ref_width / frame.shape[1]))
-                    bot = int(bot / (ref_height / frame.shape[0]))
-                    crop = frame[top:bot, left:right]
-                    if args.image_shape is not None:
-                       crop = img_as_ubyte(resize(crop, args.image_shape, anti_aliasing=True))
-                    entry['frames'].append(crop)
-    except imageio.core.format.CannotReadFrameError:
-        None
+        video_id, args = data
+        if not os.path.exists(os.path.join(args.video_folder, video_id.split('#')[0] + '.mp4')):
+            download(video_id.split('#')[0], args)
 
-    for entry in all_chunks_dict:
-        if 'person_id' in df:
-            first_part = df['person_id'].iloc[0] + "#"
+            if not os.path.exists(os.path.join(args.video_folder, video_id.split('#')[0] + '.mp4')):
+                print ('Can not load video %s, broken link' % video_id.split('#')[0])
+                return 
         else:
-            first_part = ""
-        first_part = first_part + '#'.join(video_id.split('#')[::-1])
-        path = first_part + '#' + str(entry['start']).zfill(6) + '#' + str(entry['end']).zfill(6) + '.mp4'
-        save(os.path.join(args.out_folder, partition, path), entry['frames'], args.format)
+            print("found old video")
+        reader = imageio.get_reader(os.path.join(args.video_folder, video_id.split('#')[0] + '.mp4'))
+        fps = reader.get_meta_data()['fps']
 
+        df = pd.read_csv(args.metadata)
+        df = df[df['video_id'] == video_id]
+        
+        all_chunks_dict = [{'start': df['start'].iloc[j], 'end': df['end'].iloc[j],
+                            'bbox': list(map(int, df['bbox'].iloc[j].split('-'))), 'frames':[]} for j in range(df.shape[0])]
+        ref_fps = df['fps'].iloc[0]
+        ref_height = df['height'].iloc[0]
+        ref_width = df['width'].iloc[0]
+        partition = df['partition'].iloc[0]
+        try:
+            for i, frame in enumerate(reader):
+                for entry in all_chunks_dict:
+                    if (i * ref_fps >= entry['start'] * fps) and (i * ref_fps < entry['end'] * fps):
+                        left, top, right, bot = entry['bbox']
+                        left = int(left / (ref_width / frame.shape[1]))
+                        top = int(top / (ref_height / frame.shape[0]))
+                        right = int(right / (ref_width / frame.shape[1]))
+                        bot = int(bot / (ref_height / frame.shape[0]))
+                        crop = frame[top:bot, left:right]
+                        if args.image_shape is not None:
+                            crop = img_as_ubyte(resize(crop, args.image_shape, anti_aliasing=True))
+                        entry['frames'].append(crop)
+        except imageio.core.format.CannotReadFrameError:
+            None
+
+        for entry in all_chunks_dict:
+            if 'person_id' in df:
+                first_part = df['person_id'].iloc[0] + "#"
+            else:
+                first_part = ""
+            first_part = first_part + '#'.join(video_id.split('#')[::-1])
+            path = first_part + '#' + str(entry['start']).zfill(6) + '#' + str(entry['end']).zfill(6) + '.mp4'
+            save(os.path.join(args.out_folder, partition, path), entry['frames'], args.format)
+    except Exception as e:
+        print(f"failed {e}")
 
 if __name__ == "__main__":
+    """
+    python load_videos.py --metadata ted-metadata.csv --format .png --out_folder D:\ted --workers 8 --youtube .\youtube-dl.exe
+    """
     parser = ArgumentParser()
-    parser.add_argument("--video_folder", default='youtube-taichi', help='Path to youtube videos')
-    parser.add_argument("--metadata", default='taichi-metadata-new.csv', help='Path to metadata')
-    parser.add_argument("--out_folder", default='taichi-png', help='Path to output')
+    parser.add_argument("--video_folder", default='video_folder', help='Path to youtube videos')
+    parser.add_argument("--metadata", default='ted-metadata.csv', help='Path to metadata')
+    parser.add_argument("--out_folder", default='D:/ted', help='Path to output')
     parser.add_argument("--format", default='.png', help='Storing format')
-    parser.add_argument("--workers", default=1, type=int, help='Number of workers')
-    parser.add_argument("--youtube", default='./youtube-dl', help='Path to youtube-dl')
+    parser.add_argument("--workers", default=-1, type=int, help='Number of workers')
+    parser.add_argument("--youtube", default='.\youtube-dl.exe', help='Path to youtube-dl')
  
-    parser.add_argument("--image_shape", default=(256, 256), type=lambda x: tuple(map(int, x.split(','))),
+    parser.add_argument("--image_shape", default=(512, 512), type=lambda x: tuple(map(int, x.split(','))),
                         help="Image shape, None for no resize")
 
     args = parser.parse_args()
@@ -95,7 +111,14 @@ if __name__ == "__main__":
 
     df = pd.read_csv(args.metadata)
     video_ids = set(df['video_id'])
-    pool = Pool(processes=args.workers)
+
+        
+    nber_workers=args.workers
+    if nber_workers==-1:
+        import multiprocessing
+        nber_workers=multiprocessing.cpu_count()
+
+    pool = Pool(processes=nber_workers)
     args_list = cycle([args])
     for chunks_data in tqdm(pool.imap_unordered(run, zip(video_ids, args_list))):
         None  
